7a7d18589128557ace5288a1e183252f6fae15cc
Kevin Cormier <kcormier@redhat.com>
2024-11-20 17:27:24 -0500

    Move common code out of subscription ControlData
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/lib/temptifly-utils.ts
+++ frontend/src/lib/temptifly-utils.ts
@@ -1,5 +1,47 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
+import { TFunction } from 'react-i18next'
+import { listProjects } from '../resources'
+
 export function getControlByID(controlData: { id: string }[], id: string): any | undefined {
   return controlData.find(({ id: identifier }) => identifier === id)
+}
+
+export const loadExistingNamespaces = (t: TFunction) => {
+  return {
+    query: () => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const namespaces = await listProjects().promise
+          resolve(namespaces)
+        } catch (err) {
+          reject(err)
+        }
+      })
+    },
+    loadingDesc: t('Loading namespaces...'),
+    setAvailable: setAvailableNSSpecs.bind(null),
+  }
+}
+
+export const setAvailableNSSpecs = (control: any, result: any) => {
+  const { loading } = result
+  const { data } = result
+  control.isLoading = false
+  const error = data ? null : result.error
+  if (!control.available) {
+    control.available = []
+    control.availableMap = {}
+  }
+  if (control.available.length === 0 && (error || data)) {
+    if (error) {
+      control.isFailed = true
+    } else if (data) {
+      control.isLoaded = true
+      control.available = data.map((d) => d.metadata.name)
+      control.available.sort()
+    }
+  } else {
+    control.isLoading = loading
+  }
 }
